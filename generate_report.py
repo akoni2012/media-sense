7dfb41f5652c46999d6bc6d287098e38d7ca0fe1
akoni2012 <kol395@hotmail.com>
2023-11-09 01:49:42 +0000

    Add files via upload

--- generate_report.py
+++ generate_report.py
@@ -1,4 +1,5 @@
-import pandas as pd
+
+from rules_engine import generate_ratings
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -16,13 +17,14 @@ documents = loader.load()
 embeddings = OpenAIEmbeddings()
 db = FAISS.from_documents(documents, embeddings)
 
-
 # 2. Function for similarity search
-# Identify the top k most relevant documents (recommendations) based on the query (rating) entered
+# identify the top k most relevant documents based on the query (rating, in our case) entered
 def retrieve_info(query):
     similar_response = db.similarity_search(query, k=3)
 
     page_contents_array = [doc.page_content for doc in similar_response]
+
+    # print(page_contents_array)
 
     return page_contents_array
 
@@ -32,7 +34,7 @@ llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
 
 template = """
 You are a world class Data Analyst at a Media Agency. 
-I will share a rating with you and you will give me the best recommendation
+I will share a rating with you and you will give me the best recommendation 
 to provide in a report based on best practice, 
 and you will follow ALL of the rules below:
 
@@ -41,7 +43,7 @@ in terms of length, tone of voice, logical arguments and other details
 
 2/ If the recommendations are irrelevant, then try to mimic the style of the recommendation to the rating given
 
-Below is a rating I received from a prospect:
+Below is a rating I received from the prospect:
 {rating}
 
 Here is a list of recommendations of how we normally respond to ratings in similar scenarios:
@@ -65,37 +67,14 @@ def generate_response(rating):
     return response
 
 
-# 5. Generate the ratings based on simple heuristics (normalization of feature metrics)
-def generate_ratings():
-    # read the contents of the performance metrics into a Pandas data frame
-    df = pd.read_csv('ad-verification-performance-data-italy.csv')
-    features = ['Viewability', 'Brand Safety Risk', 'IVT', 'Out-of-Geo']
-
-    # convert percentages (str) into numbers/decimals for processing
-    for i in features:
-        replace_with_floats = df[i].str.rstrip("%").astype(float)/100
-        df[i] = replace_with_floats
-
-    # Standardize the values (based on their individual benchmarks) of the features in order to make them relatable.
-    # Achieved by subtracting the benchmark from the grand total of each dimension (e.g. viewability dimension) and 
-    # then dividing the result by the benchmark. Multiply by -100 or +100 in order to standardise to whole +ive numbers
-    # which are meaningful as ratings. Returns a list of ratings for all dimensions.
-    viewability = 100*(df.loc[5]['Viewability'] - df.loc[6]['Viewability'])/df.loc[6]['Viewability']
-    brand_safety_risk = -100*(df.loc[5]['Brand Safety Risk'] - df.loc[6]['Brand Safety Risk'])/df.loc[6]['Brand Safety Risk']
-    invalid_traffic = -100*(df.loc[5]['IVT'] - df.loc[6]['IVT'])/df.loc[6]['IVT']
-    out_of_geo = -100*(df.loc[5]['Out-of-Geo'] - df.loc[6]['Out-of-Geo'])/df.loc[6]['Out-of-Geo']
-
-    rating = {'viewability': round(viewability), 'brand_safety_risk': round(brand_safety_risk), 'invalid_traffic': round(invalid_traffic), 'out_of_geo': round(out_of_geo)}
-    return rating
-
-
-# 6. Test the application
+# 5. Test the application
 def main():
 
-    rating = generate_ratings()
+    rating = generate_ratings('ad-verification-performance-data-italy.csv')
     # rating_to_string = str({'viewability': 2, 'brand_safety_risk': 31, 'invalid_traffic': 51, 'out_of_geo': 34})
     rating_to_string = str(rating)
     result = generate_response(rating_to_string)
+    print("inside main")
     print(rating_to_string)
     print(result)
 
